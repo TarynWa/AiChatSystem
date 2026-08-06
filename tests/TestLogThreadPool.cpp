0bed1d7b03a838eed754e0164471ac633f1be9d2
wangt <3187209099@qq.com>
2026-08-06 22:58:29 +0800

    链接网路

--- tests/TestLogThreadPool.cpp
+++ tests/TestLogThreadPool.cpp
@@ -1,56 +1,70 @@
-#include <iostream>
-#include <fstream>
-#include <filesystem>
-#include <atomic>
-#include <thread>
-#include <chrono>
-#include "Logger.hpp"
-#include "FixedThreadPool.hpp"
+// #include <iostream>
+// #include <fstream>
+// #include <filesystem>
+// #include <atomic>
+// #include <thread>
+// #include <chrono>
+// #include "Logger.hpp"
+// #include "FixedThreadPool.hpp"
 
-int main() {
-    namespace fs = std::filesystem;
-    fs::create_directories("logmsg");
-    std::ofstream logfile("logmsg/test_log.txt", std::ios::out | std::ios::app);
-    auto outputFn = [&logfile](const std::string &msg) {
-        if (logfile.is_open()) {
-            logfile << msg;
-        } else {
-            std::cout << msg;
-        }
-    };
-    auto flushFn = [&logfile]() {
-        if (logfile.is_open()) {
-            logfile.flush();
-        } else {
-            std::cout.flush();
-        }
-    };
+// int main() {
+//     namespace fs = std::filesystem;
+//     fs::create_directories("logmsg");
+//     std::ofstream logfile("logmsg/test_log.txt", std::ios::out | std::ios::app);
+//     auto outputFn = [&logfile](const std::string &msg) {
+//         if (logfile.is_open()) {
+//             logfile << msg;
+//         } else {
+//             std::cout << msg;
+//         }
+//     };
+//     auto flushFn = [&logfile]() {
+//         if (logfile.is_open()) {
+//             logfile.flush();
+//         } else {
+//             std::cout.flush();
+//         }
+//     };
 
-    Logger::setOutput(outputFn);
-    Logger::setFlush(flushFn);
-    Logger::setLogLevel(INFO);
+//     Logger::setOutput(outputFn);
+//     Logger::setFlush(flushFn);
+//     Logger::setLogLevel(INFO);
 
-    LOG_INFO << "Starting log + threadpool test";
+//     LOG_INFO << "Starting log + threadpool test";
 
-    FixedThreadPool pool(2, 10);
-    std::atomic<int> counter{0};
-    const int taskCount = 5;
+//     FixedThreadPool pool(2, 10);
+//     std::atomic<int> counter{0};
+//     const int taskCount = 5;
 
-    for (int i = 0; i < taskCount; ++i) {
-        pool.addTask([i, &counter]() {
-            LOG_INFO << "Task " << i << " running";
-            counter.fetch_add(1, std::memory_order_relaxed);
-        });
-    }
+//     for (int i = 0; i < taskCount; ++i) {
+//         pool.addTask([i, &counter]() {
+//             LOG_INFO << "Task " << i << " running";
+//             counter.fetch_add(1, std::memory_order_relaxed);
+//         });
+//     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    pool.stop();
+//     std::this_thread::sleep_for(std::chrono::milliseconds(200));
+//     pool.stop();
 
-    if (counter.load(std::memory_order_relaxed) != taskCount) {
-        std::cerr << "Task execution failed, counter = " << counter << std::endl;
+//     if (counter.load(std::memory_order_relaxed) != taskCount) {
+//         std::cerr << "Task execution failed, counter = " << counter << std::endl;
+//         return 1;
+//     }
+
+//     LOG_INFO << "All tasks completed, counter = " << counter;
+//     return 0;
+// }
+
+#include"chatserver.hpp"
+int main(int argc , char*argv[]){
+    if(argc<3){
+         std::cerr<<"Usage: "<<argv[0]<<" <ip> <port>"<<std::endl;
         return 1;
     }
-
-    LOG_INFO << "All tasks completed, counter = " << counter;
+    EventLoop loop;
+    InetAddress addr(argv[1],atoi(argv[2]));
+    ChatServer server(&loop,addr,"Chatserver");
+    server.start();
+    loop.loop();
     return 0;
 }
